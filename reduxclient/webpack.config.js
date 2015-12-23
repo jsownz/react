c413f6bcd3070bc8ee71774e08785c836cca2bd6
Jason Sohl <jason@frozensoliddesigns.com>
2015-12-23 14:31:32 -0800

    more react tutorial stuff - client side with unit tests

--- reduxclient/webpack.config.js
+++ reduxclient/webpack.config.js
@@ -1,6 +1,10 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
-    './src/index.js'
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.jsx'
   ],
   output: {
     path: __dirname + '/dist',
@@ -11,13 +15,17 @@ module.exports = {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'react-hot!babel'
     }]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
