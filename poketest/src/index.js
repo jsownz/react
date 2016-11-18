333ca22331637b094123fcdcc6f0d38db0742a28
Jason Sohl <jason@frozensoliddesigns.com>
2016-11-18 12:49:01 -0800

    pokedex test app

---
+++ poketest/src/index.js
@@ -0,0 +1,71 @@
+import {render} from 'react-dom';
+import React, {Component} from 'react';
+import fetch from 'isomorphic-fetch';
+
+// pokemon component
+class Pokemon extends Component {
+  render() {
+    const {pokemon, id} = this.props;
+    return <div className="pokemon--species">
+            <div className="pokemon--species--container">
+              <div className="pokemon--species--sprite">
+                <img src={`/public/sprites/${id}.png`} />
+              </div>
+              <div className="pokemon--species--name"> {pokemon.name} </div>
+            </div>
+          </div>;
+  }
+}
+
+// pokemon list component
+class PokemonList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      species: [],
+      fetched: false,
+      loading: false,
+    };
+  }
+
+  componentWillMount() {
+    this.setState({
+      loading: true
+    });
+    fetch('http://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(res=>res.json())
+      .then(response=>{
+        this.setState({
+          species: response.results,
+          loading: true,
+          fetched: true,
+        });
+      });
+  }
+
+  render() {
+    const {fetched, loading, species} = this.state;
+    let content;
+    if (fetched) {
+      content = <div className="pokemon--species--list">{species.map((pokemon,index)=><Pokemon key={pokemon.name} id={index+1} pokemon={pokemon}/>)}</div>;
+    } else if (loading && !fetched) {
+      content = <p> Loading... </p>;
+    } else {
+      content = <div />
+    }
+
+    return <div> {content} </div>;
+  }
+}
+
+// root component
+class PokeApp extends Component {
+  render() {
+    return <div className="pokeapp">
+      <h1>The Pokedex!</h1>
+      <PokemonList />
+    </div>;
+  }
+}
+
+render(<PokeApp />, document.getElementById("app"));
